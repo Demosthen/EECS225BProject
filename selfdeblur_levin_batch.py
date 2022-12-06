10fee6eb5ebdd95b85adc9889ceafe83983bc5b5
Clare Lin <2cyl@berkeley.edu>
2022-12-05 16:40:32 -0800

    batching

--- selfdeblur_levin_batch.py
+++ selfdeblur_levin_batch.py
@@ -20,26 +20,26 @@ from SSIM import SSIM
 import dataloader
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--num_iter', type=int, default=5000,
+parser.add_argument('--num_iter', type=int, default=50,
                     help='number of epochs of training')
 parser.add_argument('--img_size', type=int,
                     default=[256, 256], help='size of each image dimension')
 parser.add_argument('--kernel_size', type=int,
-                    default=[21, 21], help='size of blur kernel [height, width]')
+                    default=[27, 27], help='size of blur kernel [height, width]')
 parser.add_argument('--data_path', type=str,
                     default="datasets/test_data_loader/", help='path to blurry images')
 parser.add_argument('--batch_size', type=int,
-                    default=64, help='number of images in batch')
+                    default=16, help='number of images in batch')
 parser.add_argument('--save_path', type=str,
                     default="results/levin/hnet_models/", help='path to save results')
 parser.add_argument('--save_frequency', type=int,
-                    default=100, help='lfrequency to save results')
+                    default=10, help='lfrequency to save results')
 opt = parser.parse_args()
 # print(opt)
 
-torch.backends.cudnn.enabled = True
-torch.backends.cudnn.benchmark = True
-dtype = torch.cuda.FloatTensor
+torch.backends.cudnn.enabled = False
+torch.backends.cudnn.benchmark = False
+dtype = torch.FloatTensor
 
 warnings.filterwarnings("ignore")
 
@@ -59,122 +59,111 @@ dataloader = dataloader.get_dataloader(
 for i, (rgb, gt, rgb_path) in enumerate(dataloader):
     print(f"Processing batch: {i+1}")
 
-    for n in range(opt.batch_size):
-        path_to_image = rgb_path[n]
-        imgname = os.path.basename(path_to_image)
-        imgname = os.path.splitext(imgname)[0]
-        print("Image: ", imgname)
+    # for n in range(opt.batch_size):
+    #     print("Image: batch {0}, image {1}".format(i+1, n+1))
 
-        if imgname.find('kernel1') != -1:
-            opt.kernel_size = [17, 17]
-        if imgname.find('kernel2') != -1:
-            opt.kernel_size = [15, 15]
-        if imgname.find('kernel3') != -1:
-            opt.kernel_size = [13, 13]
-        if imgname.find('kernel4') != -1:
-            opt.kernel_size = [27, 27]
-        if imgname.find('kernel5') != -1:
-            opt.kernel_size = [11, 11]
-        if imgname.find('kernel6') != -1:
-            opt.kernel_size = [19, 19]
-        if imgname.find('kernel7') != -1:
-            opt.kernel_size = [21, 21]
-        if imgname.find('kernel8') != -1:
-            opt.kernel_size = [21, 21]
+    # _, imgs = get_image(path_to_image, -1)  # load image and convert to np.
+    # y = np_to_torch(rgb).type(dtype)
+    y = rgb
 
-        _, imgs = get_image(path_to_image, -1)  # load image and convert to np.
-        y = np_to_torch(imgs).type(dtype)
+    img_size = rgb.shape
+    # ######################################################################
+    padh, padw = opt.kernel_size[0]-1, opt.kernel_size[1]-1
+    opt.img_size[0], opt.img_size[1] = img_size[2]+padh, img_size[3]+padw
 
-        img_size = imgs.shape
-        print(imgname)
-        # ######################################################################
-        padh, padw = opt.kernel_size[0]-1, opt.kernel_size[1]-1
-        opt.img_size[0], opt.img_size[1] = img_size[1]+padh, img_size[2]+padw
+    '''
+    x_net:
+    '''
+    input_depth = 8
 
-        '''
-        x_net:
-        '''
-        input_depth = 8
+    net_input = get_noise(input_depth, INPUT,
+                          (opt.img_size[0], opt.img_size[1])).type(dtype)
 
-        net_input = get_noise(input_depth, INPUT,
-                              (opt.img_size[0], opt.img_size[1])).type(dtype)
+    net = skip(input_depth, 1,
+               num_channels_down=[128, 128, 128, 128, 128],
+               num_channels_up=[128, 128, 128, 128, 128],
+               num_channels_skip=[16, 16, 16, 16, 16],
+               upsample_mode='bilinear',
+               need_sigmoid=True, need_bias=True, pad=pad, act_fun='LeakyReLU')
 
-        net = skip(input_depth, 1,
-                   num_channels_down=[128, 128, 128, 128, 128],
-                   num_channels_up=[128, 128, 128, 128, 128],
-                   num_channels_skip=[16, 16, 16, 16, 16],
-                   upsample_mode='bilinear',
-                   need_sigmoid=True, need_bias=True, pad=pad, act_fun='LeakyReLU')
+    net = net.type(dtype)
 
-        net = net.type(dtype)
+    '''
+    k_net:
+    '''
+    n_k = 200
+    net_input_kernel = get_noise(n_k, INPUT, (1, 1)).type(dtype)
+    net_input_kernel.squeeze_()
 
-        '''
-        k_net:
-        '''
-        n_k = 200
-        net_input_kernel = get_noise(n_k, INPUT, (1, 1)).type(dtype)
-        net_input_kernel.squeeze_()
+    net_kernel = fcn(n_k, opt.kernel_size[0]*opt.kernel_size[1])
+    net_kernel = net_kernel.type(dtype)
 
-        net_kernel = fcn(n_k, opt.kernel_size[0]*opt.kernel_size[1])
-        net_kernel = net_kernel.type(dtype)
+    # Losses
+    mse = torch.nn.MSELoss().type(dtype)
+    ssim = SSIM().type(dtype)
 
-        # Losses
-        mse = torch.nn.MSELoss().type(dtype)
-        ssim = SSIM().type(dtype)
+    # optimizer
+    optimizer = torch.optim.Adam([{'params': net.parameters()}, {
+        'params': net_kernel.parameters(), 'lr': 1e-4}], lr=LR)
+    scheduler = MultiStepLR(optimizer, milestones=[
+                            2000, 3000, 4000], gamma=0.5)  # learning rates
 
-        # optimizer
-        optimizer = torch.optim.Adam([{'params': net.parameters()}, {
-            'params': net_kernel.parameters(), 'lr': 1e-4}], lr=LR)
-        scheduler = MultiStepLR(optimizer, milestones=[
-                                2000, 3000, 4000], gamma=0.5)  # learning rates
+    # initilization inputs
+    net_input_saved = net_input.detach().clone()
+    net_input_kernel_saved = net_input_kernel.detach().clone()
 
-        # initilization inputs
-        net_input_saved = net_input.detach().clone()
-        net_input_kernel_saved = net_input_kernel.detach().clone()
+    # start SelfDeblur
+    for step in tqdm(range(num_iter)):
 
-        # start SelfDeblur
-        for step in tqdm(range(num_iter)):
+        # input regularization
+        net_input = net_input_saved + reg_noise_std * \
+            torch.zeros(net_input_saved.shape).type_as(
+                net_input_saved.data).normal_()
 
-            # input regularization
-            net_input = net_input_saved + reg_noise_std * \
-                torch.zeros(net_input_saved.shape).type_as(
-                    net_input_saved.data).normal_()
+        # change the learning rate
+        scheduler.step(step)
+        optimizer.zero_grad()
 
-            # change the learning rate
-            scheduler.step(step)
-            optimizer.zero_grad()
+        # get the network output
+        out_x = net(net_input)
+        out_k = net_kernel(net_input_kernel)
 
-            # get the network output
-            out_x = net(net_input)
-            out_k = net_kernel(net_input_kernel)
+        out_k_m = out_k.view(-1, 1, opt.kernel_size[0], opt.kernel_size[1])
+        # print(out_k_m)
+        out_y = nn.functional.conv2d(out_x, out_k_m, padding=0, bias=None)
 
-            out_k_m = out_k.view(-1, 1, opt.kernel_size[0], opt.kernel_size[1])
-            # print(out_k_m)
-            out_y = nn.functional.conv2d(out_x, out_k_m, padding=0, bias=None)
+        if step < 1000:
+            total_loss = mse(out_y, y)
+        else:
+            total_loss = 1-ssim(out_y, y)
 
-            if step < 1000:
-                total_loss = mse(out_y, y)
-            else:
-                total_loss = 1-ssim(out_y, y)
+        total_loss.backward()
+        optimizer.step()
 
-            total_loss.backward()
-            optimizer.step()
+        # print the loss
+        if step % 10 == 0:
+            print("{}: {}".format(step, total_loss.item()))
 
-            if (step+1) % opt.save_frequency == 0:
-                #print('Iteration %05d' %(step+1))
+        if (step+1) % opt.save_frequency == 0:
+            #print('Iteration %05d' %(step+1))
+
+            for n in range(opt.batch_size):
+                path_to_image = rgb_path[n]
+                imgname = os.path.basename(path_to_image)
+                imgname = os.path.splitext(imgname)[0]
 
                 save_path = os.path.join(opt.save_path, '%s_x.png' % imgname)
                 out_x_np = torch_to_np(out_x)
                 out_x_np = out_x_np.squeeze()
                 out_x_np = out_x_np[padh//2:padh//2 +
-                                    img_size[1], padw//2:padw//2+img_size[2]]
-                imsave(save_path, out_x_np)
+                                    img_size[2], padw//2:padw//2+img_size[3]]
+                imsave(save_path, out_x_np.astype(np.uint8))
 
                 save_path = os.path.join(opt.save_path, '%s_k.png' % imgname)
                 out_k_np = torch_to_np(out_k_m)
                 out_k_np = out_k_np.squeeze()
                 out_k_np /= np.max(out_k_np)
-                imsave(save_path, out_k_np)
+                imsave(save_path, out_k_np.astype(np.uint8))
 
                 torch.save(net, os.path.join(
                     opt.save_path, "%s_xnet.pth" % imgname))
