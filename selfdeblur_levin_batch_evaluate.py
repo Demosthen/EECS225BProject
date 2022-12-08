4eb5805724ab96df2424bdda208dbc19f4eb1a63
AaronGuo213 <aaronguo213@gmail.com>
2022-12-08 14:04:26 -0800

    evaluation function additions

--- selfdeblur_levin_batch_evaluate.py
+++ selfdeblur_levin_batch_evaluate.py
@@ -19,15 +19,16 @@ from torch.optim.lr_scheduler import MultiStepLR
 import torch.nn.functional as F
 from utils.common_utils import *
 from SSIM import SSIM
-import dataloader
+from dataloader import get_dataloader
 import wandb
 from statistics import psnr, ssim
 
-def evaluate_hnet(hyper_dip, hyper_fcn, net, net_kernel, img_size, kernel_size, n_k, iterations):
+def evaluate_hnet(opt, hyper_dip, hyper_fcn, net, net_kernel, n_k, iterations):
     output_img = 0
-    data_path = "datasets/test_data_loader/"
-    save_path = "results/levin/hnet_evaluation/"
+    validation_data_path = "datasets/test_data_loader/"
+    validation_save_path = "results/levin/hnet_evaluation/"
     INPUT = 'noise'
+    reg_noise_std = 0.001
 
     if torch.cuda.is_available():
         torch.backends.cudnn.enabled = True
@@ -38,11 +39,12 @@ def evaluate_hnet(hyper_dip, hyper_fcn, net, net_kernel, img_size, kernel_size, 
         torch.backends.cudnn.benchmark = False
         dtype = torch.FloatTensor
 
-    dataloader = dataloader.get_dataloader(
-        data_path, batch_size=32, shuffle=False)
+    dataloader = get_dataloader(
+        validation_data_path, batch_size=32, shuffle=False)
     print(f"Evaluating HNet")
 
-    for i, (rgb, gt, rgb_path) in enumerate(dataloader):
+    iterator = iter(dataloader)
+    for i, (rgb, gt, rgb_path) in enumerate(iterator):
         # Get our current batch size since it could be less than opt.batch_size
         batch_size = len(rgb)
 
@@ -51,8 +53,8 @@ def evaluate_hnet(hyper_dip, hyper_fcn, net, net_kernel, img_size, kernel_size, 
 
         img_size = rgb.shape
         # ######################################################################
-        padh, padw = kernel_size[0]-1, kernel_size[1]-1
-        img_size[0], img_size[1] = img_size[2]+padh, img_size[3]+padw
+        padh, padw = opt.kernel_size[0]-1, opt.kernel_size[1]-1
+        opt.img_size[0], opt.img_size[1] = img_size[2]+padh, img_size[3]+padw
 
         '''
         x_net:
@@ -74,8 +76,7 @@ def evaluate_hnet(hyper_dip, hyper_fcn, net, net_kernel, img_size, kernel_size, 
 
         # optimizer
         optimizer = torch.optim.Adam([{'params': net.parameters()}, {
-            'params': net_kernel.parameters(), 'lr': 1e-4},
-            {'params': hyper_dip.parameters()}, {'params': hyper_fcn.parameters()}], lr=LR)
+                                    'params': net_kernel.parameters(), 'lr': 1e-4}], lr=LR)
         scheduler = MultiStepLR(optimizer, milestones=[
                                 2000, 3000, 4000], gamma=0.5)  # learning rates
 
@@ -90,10 +91,39 @@ def evaluate_hnet(hyper_dip, hyper_fcn, net, net_kernel, img_size, kernel_size, 
         psnr_total = 0
         mse_total = 0
 
+        ### train SelfDeblur
+        for step in tqdm(range(iterations)):
+
+            # input regularization
+            net_input = net_input_saved + reg_noise_std * \
+                torch.zeros(net_input_saved.shape).type_as(
+                    net_input_saved.data).normal_()
+
+            # change the learning rate
+            scheduler.step(step)
+            optimizer.zero_grad()
+
+            # get the network output
+            out_x = net(net_input)
+            out_k = net_kernel(net_input_kernel)
+
+            out_k_m = out_k.view(-1, 1, opt.kernel_size[0], opt.kernel_size[1])
+            # print(out_k_m)
+            out_y = nn.functional.conv2d(out_x, out_k_m, padding=0, bias=None)
+
+            if step < 1000:
+                total_loss = mse(out_y, y)
+            else:
+                total_loss = 1-ssim(out_y, y)
+
+            total_loss.backward()
+            optimizer.step()
+
+        # evaluate trained selfdeblur
         for i, img in enumerate(rgb):
             out_x = net(net_input, weights=dip_weights[i])
             out_k = net_kernel(net_input_kernel, fcn_weights[i]) 
-            out_k_m = out_k.view(-1, 1, kernel_size[0], kernel_size[1])
+            out_k_m = out_k.view(-1, 1, opt.kernel_size[0], opt.kernel_size[1])
             psnr_total += psnr(out_x, y)
             mse_total += mse(out_x, y)
             if i == output_img:
@@ -101,23 +131,23 @@ def evaluate_hnet(hyper_dip, hyper_fcn, net, net_kernel, img_size, kernel_size, 
                 imgname = os.path.basename(path_to_image)
                 imgname = os.path.splitext(imgname)[0]
 
-                save_path = os.path.join(save_path, '%s_x.png' % imgname)
+                validation_save_path = os.path.join(validation_save_path, '%s_x.png' % imgname)
                 out_x_np = torch_to_np(out_x)
                 out_x_np = out_x_np.squeeze()
                 out_x_np = out_x_np[padh//2:padh//2 +
                                     img_size[2], padw//2:padw//2+img_size[3]]
-                imsave(save_path, out_x_np.astype(np.uint8))
+                imsave(validation_save_path, out_x_np.astype(np.uint8))
 
-                save_path = os.path.join(save_path, '%s_k.png' % imgname)
+                validation_save_path = os.path.join(validation_save_path, '%s_k.png' % imgname)
                 out_k_np = torch_to_np(out_k_m)
                 out_k_np = out_k_np.squeeze()
                 out_k_np /= np.max(out_k_np)
-                imsave(save_path, out_k_np.astype(np.uint8))
+                imsave(validation_save_path, out_k_np.astype(np.uint8))
 
                 torch.save(net, os.path.join(
-                    save_path, "%s_xnet.pth" % imgname))
+                    validation_save_path, "%s_xnet.pth" % imgname))
                 torch.save(net_kernel, os.path.join(
-                    save_path, "%s_knet.pth" % imgname))
+                    validation_save_path, "%s_knet.pth" % imgname))
                 to_log["img"] = wandb.Image(out_x_np, mode="L")
                 to_log["kernel"] = wandb.Image(out_k_np, mode="L")
         psnr_average = psnr_total / len(rgb)
@@ -129,3 +159,66 @@ def evaluate_hnet(hyper_dip, hyper_fcn, net, net_kernel, img_size, kernel_size, 
         }
 
         wandb.log(to_log)
+
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--num_epochs', type=int, default=50,
+                    help='number of epochs of training')
+parser.add_argument('--num_iter', type=int, default=50,
+                    help='number of iterations per image')
+parser.add_argument('--img_size', type=int,
+                    default=[256, 256], help='size of each image dimension')
+parser.add_argument('--kernel_size', type=int,
+                    default=[27, 27], help='size of blur kernel [height, width]')
+parser.add_argument('--data_path', type=str,
+                    default="datasets/test_data_loader/", help='path to blurry images')
+parser.add_argument('--batch_size', type=int,
+                    default=16, help='number of images in batch')
+parser.add_argument('--save_path', type=str,
+                    default="results/levin/hnet_models/", help='path to save results')
+parser.add_argument('--save_frequency', type=int,
+                    default=10, help='lfrequency to save results')
+parser.add_argument('--l1_coeff', type=float,
+                    default=0, help="coefficient on L1 norm of kernel in loss function")
+opt = parser.parse_args()
+
+# testing evaluate_hnet
+if torch.cuda.is_available():
+    torch.backends.cudnn.enabled = True
+    torch.backends.cudnn.benchmark = True
+    dtype = torch.cuda.FloatTensor
+else:
+    torch.backends.cudnn.enabled = False
+    torch.backends.cudnn.benchmark = False
+    dtype = torch.FloatTensor
+
+INPUT = 'noise'
+pad = 'reflection'
+LR = 0.01
+KERNEL_LR= 0.01
+num_iter = 5000
+reg_noise_std = 0.001
+input_depth = 8
+
+
+net = HyperDip(input_depth, 1,
+               num_channels_down=[128, 128, 128, 128, 128],
+               num_channels_up=[128, 128, 128, 128, 128],
+               num_channels_skip=[16, 16, 16, 16, 16],
+               upsample_mode='bilinear',
+               need_sigmoid=True, need_bias=True, pad='reflection', act_fun='LeakyReLU')
+net = net.type(dtype)
+
+n_k = 200
+net_kernel = HyperFCN(n_k, opt.kernel_size[0]*opt.kernel_size[1])
+net_kernel = net_kernel.type(dtype)
+
+hyper_dip = HyperNetwork(net)
+hyper_dip = hyper_dip.type(dtype)
+
+hyper_fcn = HyperNetwork(net_kernel)
+hyper_fcn = hyper_fcn.type(dtype)
+
+evaluate_hnet(opt, hyper_dip, hyper_fcn, net, net_kernel, n_k, 5000)
